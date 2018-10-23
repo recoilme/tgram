1c0a54c2d2d2adbfd7cf08fe46f17a525d61b0e5
Vadim Kilibaba <vkulibaba@surfy.ru>
2018-10-23 18:07:59 +0300

    disable CORS, round 2

--- main.go
+++ main.go
@@ -184,7 +184,7 @@ func InitRouter() *gin.Engine {
 // CORSMiddleware - open for request from javascript
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://tgr.am")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://crazydoge.com/")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
